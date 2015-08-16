f9641a87a67c84c61527855b8066654267cb6124
Patrick Lindsay <patrick.lindsay@sage.com>
2015-08-16 23:38:36 +0100

    Initial commit

---
+++ lib/will_paginate/materialize.rb
@@ -0,0 +1,7 @@
+require "will_paginate/materialize/version"
+
+module WillPaginate
+  module Materialize
+    # Your code goes here...
+  end
+end
---
+++ lib/will_paginate/materialize/version.rb
@@ -0,0 +1,5 @@
+module WillPaginate
+  module Materialize
+    VERSION = "0.1.0"
+  end
+end
